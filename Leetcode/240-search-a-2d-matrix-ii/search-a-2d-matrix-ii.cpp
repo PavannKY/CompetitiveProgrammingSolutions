29935207a8f435cc32abf42f0ef01c2871c40024
Pavan <160629370+PavannKY@users.noreply.github.com>
2026-01-26 18:33:03 +0530

    Time: 56 ms (44.38%) | Memory: 18.7 MB (66.77%) - LeetSync

--- Leetcode/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ Leetcode/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -3,32 +3,18 @@ public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         int row = mat.size();
         int col = mat[0].size();
-        for(int mid = 0;mid<row;mid++)
-        {
-            
-            int l = 0;
-            int h = col-1;
-            while(h>=l)
-            {
-                int m = l+(h-l)/2;
-                if(mat[mid][m] == target)
-                {
-                    return 1;
-                }
 
-                if(mat[mid][m]>target)
-                {
-                    h = m-1;
-                }
-                else
-                {
-                    l = m+1;
-                }
-            }
+        int i = 0;
+        int j = col-1;
+
+        while(i<row && j>=0)
+        {
+            if(mat[i][j] == target) return 1;
+            else if(mat[i][j] < target) i++;
+            else j--;
         }
 
         return 0;
-
         
     }
 };
