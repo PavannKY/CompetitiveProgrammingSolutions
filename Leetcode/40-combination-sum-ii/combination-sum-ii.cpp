bfba4b5d1f10749783dcdd8e9eb54806c403a416
Pavan <160629370+PavannKY@users.noreply.github.com>
2026-03-14 02:07:02 +0530

    Time: 1 ms (49.62%) | Memory: 14 MB (61.47%) - LeetSync

---
+++ Leetcode/40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void solve(int index,int target,vector<int>& candidates,vector<int>& ds,vector<vector<int>> & ans)
+    {
+        if(target==0)
+        {
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i = index;i<candidates.size();i++)
+        {
+            if(i>index && candidates[i] == candidates[i-1])
+            {
+                continue;
+            }
+
+            if(candidates[i] > target)
+            {
+                break;
+            }
+
+            ds.push_back(candidates[i]);
+
+            solve(i+1,target-candidates[i],candidates,ds,ans);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        sort(candidates.begin(),candidates.end());
+
+        solve(0,target,candidates,ds,ans);
+
+        return ans;
+
+    }
+};
