2b9fd6cd6a9a6ef2776b4f3a6d6632a57cd97a46
Pavan <160629370+PavannKY@users.noreply.github.com>
2026-04-08 02:04:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (53.67%) - LeetSync

---
+++ Leetcode/50-powx-n/powx-n.c
@@ -0,0 +1,22 @@
+double myPow(double x, long int n) {
+    if(n<0)
+    {
+        x = 1/x;
+        n = -n;
+    }
+    
+    if(n==0)
+    {
+        return 1;
+    }
+
+    if(n%2==0)
+    {
+        return myPow(x*x,n/2);
+    }
+
+    else
+    {
+        return x*myPow(x*x,n/2);
+    }
+}
